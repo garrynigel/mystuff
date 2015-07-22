1e74c24585eef39ee08b8b3262dcbdf4d0188ffb
Garry Dmello <garrynigel@gmail.com>
2015-07-22 12:20:38 -0700

    main function and structure of node

--- linkedlist.c
+++ linkedlist.c
@@ -5,3 +5,39 @@
 *****************************************************/
 #include <stdio.h>
 #include <conio.h>
+
+/*****************************************************
+
+     Define structures of each node
+
+*****************************************************/
+
+struct nodes
+{
+  int val;
+  struct *next;
+};
+
+/*****************************************************
+
+        Main Function
+
+*****************************************************/
+
+int main(int argc, char const *argv[]) {
+{
+
+  int option = 999;
+
+
+  while option != '5'
+  {
+       printf("Enter the option from the menu below");
+       printf(" 1 : Insert a new node ");
+       printf(" 2 : Delete an existing node ");
+       printf(" 3 : Display the linked list ");
+   }
+
+
+  return 0;
+}
