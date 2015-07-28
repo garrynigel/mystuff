9da556c75675716e6c5b022edc364a172bafe6be
Garry Dmello <garrynigel@gmail.com>
2015-07-28 10:12:49 -0700

    Added insert operation Function

--- linkedlist.c
+++ linkedlist.c
@@ -4,7 +4,7 @@
 
 *****************************************************/
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 
 /*****************************************************
 
@@ -15,27 +15,93 @@
 struct nodes
 {
   int val;
-  struct *next;
+  struct nodes *next;
 };
 
+/*****************************************************
+
+   Declare global pointers for head and current node
+
+*****************************************************/
+
+struct nodes *head = NULL;
+struct nodes *curr = NULL;
+
+/*****************************************************
+
+        Insert Function
+
+*****************************************************/
+
+int insopt(struct nodes *, struct nodes * )
+{
+
+  int option;
+
+  printf("Enter the insert options from the menu below\n");
+  printf("1: Insert head node\n");
+  printf("2: Insert node after\n");
+  printf("3: Insert node before\n");
+  scanf("%d\n",&option );
+
+  switch (option) {
+    case 1:
+    if (!head)
+    {
+      head->val = curr->val;
+      head->next = NULL;
+    }
+    else
+    {
+      head->next = head;
+      head->val  = curr->val;
+    }
+  }
+
+}
 /*****************************************************
 
         Main Function
 
 *****************************************************/
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
 {
 
-  int option = 999;
+  int opt = 999;
+  int insopt = 2;
+  int value ;
 
-
-  while option != '5'
+  while (opt != '4')
   {
-       printf("Enter the option from the menu below");
-       printf(" 1 : Insert a new node ");
-       printf(" 2 : Delete an existing node ");
-       printf(" 3 : Display the linked list ");
+       printf("Enter the option from the menu below\n\n");
+       printf(" 1 : Insert a new node \n");
+       printf(" 2 : Delete an existing node \n");
+       printf(" 3 : Display the linked list \n");
+       printf(" 4 : Exit \n");
+
+       switch (opt)
+       {
+         case 1 :
+         printf("Enter value of node to be inserted \n>>>> ");
+         scanf("%d",&value);
+         curr->val = value;
+         insopt(*head,*curr);
+         case 2 :
+         printf("Enter value of node to be deleted \n>>>> ");
+         case 3 :
+         printf(" The linked list as follows \n>>>> ");
+         printf("%d",head->val);
+         curr = head->next;
+         while (!curr)
+         {
+          printf("%d",curr->val);
+          curr = head->next;
+         }
+         case 4 :
+         printf("Exit");
+         break;
+       }
    }
 
 
